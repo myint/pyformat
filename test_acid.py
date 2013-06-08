d3dcfbf2658b982fd0ff318a5834a240c589f5ff
Steven Myint <git@stevenmyint.com>
2013-06-08 08:35:54 -0700

    Catch all syntax errors

--- test_acid.py
+++ test_acid.py
@@ -91,7 +91,7 @@ def check_syntax(filename, raise_error=False):
             filename,
             encoding=autopep8.detect_encoding(filename)) as input_file:
         try:
-            compile(input_file.read(), '<string>', 'exec')
+            compile(input_file.read(), '<string>', 'exec', dont_inherit=True)
             return True
         except (SyntaxError, TypeError, UnicodeDecodeError):
             if raise_error:
