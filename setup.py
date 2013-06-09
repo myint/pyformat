00b972d86fd0a8fc2e91671a3a8d7e6cd8b86caa
Steven Myint <git@stevenmyint.com>
2013-06-09 08:53:09 -0700

    Bump required versions

--- setup.py
+++ setup.py
@@ -33,9 +33,9 @@ with open('README.rst') as readme:
               'Topic :: Software Development :: Libraries :: Python Modules'],
           keywords='beautify, code, format, formatter, reformat, style',
           py_modules=['pyformat'],
-          install_requires=['autoflake>=0.4',
-                            'autopep8>=0.9',
-                            'docformatter>=0.3.2',
+          install_requires=['autoflake>=0.4.2',
+                            'autopep8>=0.9.1',
+                            'docformatter>=0.4',
                             'unify>=0.1.4'],
           scripts=['pyformat'],
           test_suite='test_pyformat')
