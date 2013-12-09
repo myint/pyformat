076f1f344f86de13d9b2f8d77a574b3fc6e6bdad
Steven Myint <git@stevenmyint.com>
2013-12-09 07:02:04 -0800

    Get ready for autopep8 1.0

--- pyformat.py
+++ pyformat.py
@@ -50,7 +50,12 @@ def formatters(aggressive):
     if aggressive:
         yield autoflake.fix_code
 
-        autopep8_options = autopep8.parse_args(['', '--aggressive'])[0]
+        autopep8_options = autopep8.parse_args(['', '--aggressive'])
+        try:
+            # For old autopep8. Remove this once autopep8 1.0 is released.
+            autopep8_options = autopep8_options[0]
+        except TypeError:  # pragma: no cover
+            pass
     else:
         autopep8_options = None
 
