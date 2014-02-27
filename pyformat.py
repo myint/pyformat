4f47ebbb3276164dcbe53d6dc58ad12c6d755837
Steven Myint <git@stevenmyint.com>
2014-02-27 07:07:58 -0800

    Increment required versions

--- pyformat.py
+++ pyformat.py
@@ -36,7 +36,7 @@ import docformatter
 import unify
 
 
-__version__ = '0.5.3'
+__version__ = '0.5.4'
 
 
 try:
--- setup.py
+++ setup.py
@@ -37,8 +37,8 @@ with open('README.rst') as readme:
           py_modules=['pyformat'],
           zip_safe=False,
           install_requires=['autoflake>=0.6.1',
-                            'autopep8>=0.9.7',
-                            'docformatter>=0.5.6',
+                            'autopep8>=1.0',
+                            'docformatter>=0.5.7',
                             'unify>=0.2'],
           entry_points={
               'console_scripts': ['pyformat = pyformat:main']},
