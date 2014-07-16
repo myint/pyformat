44253e33045e04de016eb95d7fed98c0453b835b
Steven Myint <git@stevenmyint.com>
2014-07-16 05:52:46 -0700

    Use all future imports

--- pyformat.py
+++ pyformat.py
@@ -23,6 +23,8 @@
 
 """Formats Python code to follow a consistent style."""
 
+from __future__ import absolute_import
+from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
