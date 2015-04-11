9af707df4c774b830f7d9ff67656a7280fb584c4
Steven Myint <git@stevenmyint.com>
2015-04-11 08:56:14 -0700

    List Python 3.5

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ with open('README.rst') as readme:
               'Programming Language :: Python :: 3.2',
               'Programming Language :: Python :: 3.3',
               'Programming Language :: Python :: 3.4',
+              'Programming Language :: Python :: 3.5',
               'Topic :: Software Development :: Libraries :: Python Modules',
               'Topic :: Software Development :: Quality Assurance'],
           keywords='beautify, code, format, formatter, reformat, style',
